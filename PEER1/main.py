53f1d1ad4cb9841dddd809987dc8550715f735f6
phongdang17183 <dangquocphong1703@gmail.com>
2024-11-05 18:05:34 +0700

    add upload function

--- PEER1/main.py
+++ PEER1/main.py
@@ -20,7 +20,7 @@ def main():
     # host = input("Enter Host: ")
     # port = int(input("Enter Port (for listening): "))
     host = get_host_default()
-    port = 1000
+    port = 1001
     print("hi {} {}".format(host, port))
     
     client = ClientSite(Peer(host, port))
